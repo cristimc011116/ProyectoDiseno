78e6fff0d4f5065fe780cdb68c4ebac6909ffe93
cristimc011116 <cristimc011116@gmail.com>
2022-05-01 18:20:01 -0600

    Crear cuenta listo

--- ProDS/src/main/java/CLI/CLI.java
+++ ProDS/src/main/java/CLI/CLI.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package CLI;
+import controlador.ControladorUsuario;
 import java.util.Scanner;
 import validacion.ExpresionesRegulares;
 import dao.PersonaDAO;
@@ -21,30 +22,48 @@ public class CLI {
     //public void crearCuenta()
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner (System.in);
-        boolean esNum = false;
-        boolean esNum2 = false;
-        boolean esPin = false;
+        int id = pedirId();
+        String pin = pedirPin();
+        int monto = pedirMonto();
+        int numero = ControladorUsuario.insertarCuenta(pin, monto, id);
         
+        String texto4 = "Se ha creado una nueva cuenta en el sistema, los datos de la cuenta son: ";
+        System.out.println(texto4);
+        String texto5 = ControladorUsuario.imprimirCuenta(numero);
+        System.out.println(texto5);
+        System.out.println("---");
+        String texto6 = ControladorUsuario.imprimirPersona(id);
+        System.out.println(texto6); 
+    }
+    
+    
+    
+    public static int pedirId()
+    {
+        Scanner sc = new Scanner (System.in);
         String texto = "Digite su identificacion: ";
         System.out.println(texto);
         String strid = sc.next();
-        boolean esCorrecto = pedirIdP1(strid);
+        boolean esCorrecto = ControladorUsuario.auxIdP1(strid);
         
         while (esCorrecto == false)
         {
             String texto1 = "Digite su identificacion: ";
             System.out.println(texto1);
             strid = sc.next();
-            esCorrecto = pedirIdP1(strid);
+            esCorrecto = ControladorUsuario.auxIdP1(strid);
         }
-        
         int id = Integer.parseInt(strid);
-        
+        return id;
+    }
+    
+    public static String pedirPin()
+    {
+        Scanner sc = new Scanner (System.in);
         String texto2 = "Digite el pin que será asignado a la cuenta: ";
         System.out.println(texto2);
         String pin = sc.next();
-        esPin = ExpresionesRegulares.validarPin(pin);
+        boolean esPin = ExpresionesRegulares.validarPin(pin);
         
         while (esPin == false)
         {
@@ -53,88 +72,27 @@ public class CLI {
             pin = sc.next();
             esPin = ExpresionesRegulares.validarPin(pin);
         }
-        
+        return pin;
+    }
+    
+    public static int pedirMonto()
+    {
+        Scanner sc = new Scanner (System.in);
         String texto3 = "Digite el monto de depósito inicial: ";
         System.out.println(texto3);
         String strmonto = sc.next();
-        esNum2 = ExpresionesRegulares.esNumero(strmonto);
+        boolean esNum = ExpresionesRegulares.esNumero(strmonto);
         
-        while (esNum2 == false)
+        while (esNum == false)
         {
             String texto4 = "Digite el monto de depósito inicial: ";
             System.out.println(texto4);
             strmonto = sc.next();
-            esNum2 = ExpresionesRegulares.esNumero(strmonto);
+            esNum = ExpresionesRegulares.esNumero(strmonto);
         }
         
         int monto = Integer.parseInt(strmonto);
-        
-        //Metodo controlador
-        int numero = Cuenta.generarNumCuenta();
-        LocalDate fecha = Cuenta.setFechaCreacion();
-        String strFecha = fecha.toString();
-        Cuenta cuenta = new Cuenta(numero, pin, fecha, monto, "activo");
-        CuentaDAO.insertarCuenta(cuenta,fecha);
-        CuentaDAO.asignarCuentaCliente(cuenta, id);
-        //Hasta aca
-        
-        String texto4 = "Se ha creado una nueva cuenta en el sistema, los datos de la cuenta son: ";
-        System.out.println(texto4);
-        String texto5 = imprimirCuenta(numero);
-        System.out.println(texto5);
-        System.out.println("---");
-        String texto6 = imprimirPersona(id);
-        System.out.println(texto6); 
+        return monto;
     }
     
-    public static boolean pedirIdP1(String strid){
-        boolean esCorrecto = false;
-        boolean esNum = ExpresionesRegulares.esNumero(strid);
-        if(esNum == true)
-        {
-            int id = Integer.parseInt(strid);
-            esCorrecto = pedirIdP2(id);
-        }
-        return esCorrecto;
-    }
-    
-    public static boolean pedirIdP2(int id){
-        boolean esNum = false;
-        Persona persona = PersonaDAO.obtenerPersona(id);
-        String nombre = persona.getNombre();
-        if(null != nombre){
-            esNum = true;
-        }
-        
-        return esNum;
-    }
-    
-    public static int validarId(strid)
-    {
-        boolean esCorrecto = false;
-        while (esCorrecto == false)
-        {
-            String texto1 = "Digite su identificacion: ";
-            System.out.println(texto1);
-            strid = sc.next();
-            esCorrecto = pedirIdP1(strid);
-        }
-    }
-    
-    
-    
-    //En controlador
-    public static String imprimirCuenta(int pNum){
-        Cuenta cuenta = CuentaDAO.obtenerCuenta(pNum);
-        String mensaje = "Número de cuenta: " + pNum + "\nEstatus de la cuenta: " + cuenta.getEstatus() + "\nSaldo actual: " + cuenta.getSaldo();
-        return mensaje;
-    }
-    
-    public static String imprimirPersona(int id){
-        Persona persona = PersonaDAO.obtenerPersona(id);
-        String mensaje = "Nombre del dueño de la cuenta: " + persona.getNombre() + " " + persona.getPrimerApellido() + " " + 
-                persona.getSegundoApellido() + "\nNúmero de teléfono 'asociado' a la cuenta: " + 
-                persona.getNumero() + "\nDirección de correo elctrónico 'asociada' a la cuenta: " + persona.getCorreo();
-        return mensaje;
-    }
 }
--- ProDS/src/main/java/controlador/ControladorUsuario.java
+++ ProDS/src/main/java/controlador/ControladorUsuario.java
@@ -4,10 +4,64 @@
  */
 package controlador;
 
+import dao.CuentaDAO;
+import dao.PersonaDAO;
+import java.time.LocalDate;
+import logicadenegocios.Cuenta;
+import logicadenegocios.Persona;
+import validacion.ExpresionesRegulares;
+
 /**
  *
  * @author Cristi Martínez
  */
 public class ControladorUsuario {
     
+    
+    public static int insertarCuenta(String pPin, int pMonto, int pId)
+    {
+        int numero = Cuenta.generarNumCuenta();
+        LocalDate fecha = Cuenta.setFechaCreacion();
+        Cuenta cuenta = new Cuenta(numero, pPin, fecha, pMonto, "activo");
+        CuentaDAO.insertarCuenta(cuenta,fecha);
+        CuentaDAO.asignarCuentaCliente(cuenta, pId);
+        
+        return numero;
+    }
+    
+    public static String imprimirCuenta(int pNum){
+        Cuenta cuenta = CuentaDAO.obtenerCuenta(pNum);
+        String mensaje = "Número de cuenta: " + pNum + "\nEstatus de la cuenta: " + cuenta.getEstatus() + "\nSaldo actual: " + cuenta.getSaldo();
+        return mensaje;
+    }
+    
+    public static String imprimirPersona(int id){
+        Persona persona = PersonaDAO.obtenerPersona(id);
+        String mensaje = "Nombre del dueño de la cuenta: " + persona.getNombre() + " " + persona.getPrimerApellido() + " " + 
+                persona.getSegundoApellido() + "\nNúmero de teléfono 'asociado' a la cuenta: " + 
+                persona.getNumero() + "\nDirección de correo elctrónico 'asociada' a la cuenta: " + persona.getCorreo();
+        return mensaje;
+    }
+    
+    public static boolean auxIdP1(String strid){
+        boolean esCorrecto = false;
+        boolean esNum = ExpresionesRegulares.esNumero(strid);
+        if(esNum == true)
+        {
+            int id = Integer.parseInt(strid);
+            esCorrecto = auxIdP2(id);
+        }
+        return esCorrecto;
+    }
+    
+    public static boolean auxIdP2(int id){
+        boolean esNum = false;
+        Persona persona = PersonaDAO.obtenerPersona(id);
+        String nombre = persona.getNombre();
+        if(null != nombre){
+            esNum = true;
+        }
+        
+        return esNum;
+    }
 }
