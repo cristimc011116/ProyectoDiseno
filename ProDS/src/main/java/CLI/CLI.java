12a3edb6097424c3e89a0e80465adefb2f51c43c
cristimc011116 <cristimc011116@gmail.com>
2022-05-17 16:50:18 -0600

    Revert "cambios en la consulta de comisiones CLI"
    This reverts commit bb85d344e14cec3bb4bb670cc2926bb94bc13e18.

--- ProDS/src/main/java/CLI/CLI.java
+++ ProDS/src/main/java/CLI/CLI.java
@@ -174,45 +174,27 @@ public class CLI {
     
     public static void ConsultaGananciaBanco(String opcion)
     {
-      double sumaRetiros =0;
-      double sumaDepositos =0;
-      double sumaComisiones = 0;
       //totalizado
       if ("18".equals(opcion))
       {
+        double sumaComisiones = 0;
         ArrayList<Cuenta> listaCuentas = CuentaDAO.getCuentasBD();
         Cuenta cuenta=new Cuenta();
         System.out.println("------------------------------------------------------------------------------------");
         System.out.println("|  Cuenta   |-|  Detalle de la operación|-|Fecha de la operación|-|   Monto  |-| Comisión del Banco|");
         for(int i=0;i<listaCuentas.size();i++){
           cuenta = listaCuentas.get(i);
-          sumaRetiros = Operacion.sumarComisionesRetiros(cuenta.getNumero());
-          sumaDepositos = Operacion.sumarComisionesdepositos(cuenta.getNumero());
           sumaComisiones = sumaComisiones + LlenarTablaConsultaBancoTotalizado(cuenta.getNumero());
         }
         System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("LA SUMA DE LA GANANCIA DEL BANCO POR DEPOSITOS ES DE:"+ sumaDepositos);
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("LA SUMA DE LA GANANCIA DEL BANCO POR RETIROS ES DE:"+ sumaRetiros);
-        System.out.println("------------------------------------------------------------------------------------");
         System.out.println("LA SUMA TOTAL DE LA GANANCIA DEL BANCO ES DE:"+ sumaComisiones);
-        System.out.println("------------------------------------------------------------------------------------");
         volverMenu();
       }
       //Cuenta específica:
       if ("19".equals(opcion))
       {
         String pNumCuenta = pedirNumCuenta();
-        sumaComisiones = LlenarTablaConsultaBancoPorCuenta(pNumCuenta);
-        sumaRetiros = Operacion.sumarComisionesRetiros(pNumCuenta);
-        sumaDepositos = Operacion.sumarComisionesdepositos(pNumCuenta);
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("LA SUMA DE LA GANANCIA DEL BANCO POR DEPOSITOS ES DE:"+ sumaDepositos);
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("LA SUMA DE LA GANANCIA DEL BANCO POR RETIROS ES DE:"+ sumaRetiros);
-        System.out.println("------------------------------------------------------------------------------------");
-        System.out.println("LA SUMA TOTAL DE LA GANANCIA DEL BANCO ES DE:"+ sumaComisiones);
-        System.out.println("------------------------------------------------------------------------------------");
+        LlenarTablaConsultaBancoPorCuenta(pNumCuenta);
         volverMenu();
       }
         
@@ -226,19 +208,16 @@ public class CLI {
       ArrayList<Operacion> operaciones = OperacionDAO.getOperacionesCuenta(cuentaDesencrip);
       for(Operacion operacion: operaciones)
       {
-        if (operacion.getTipo().equals("retiro") && operacion.getTipo().equals("deposito"))
-        {
-          double monto = (operacion.getMontoComision()/0.02);
-          System.out.println("------------------------------------------------------------------------------------");
-          System.out.println("|"+cuentaDesencrip+"|-|"+operacion.getTipo()+"|-|"+operacion.getFechaOperacion()+"|-|"+ monto+"|-|"+operacion.getMontoComision()+"|");
-          sumaComisiones = sumaComisiones + operacion.getMontoComision();
-        }
+        double monto = (operacion.getMontoComision()/0.02);
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("|"+cuentaDesencrip+"|-|"+operacion.getTipo()+"|-|"+operacion.getFechaOperacion()+"|-|"+ monto+"|-|"+operacion.getMontoComision()+"|");
+        sumaComisiones = sumaComisiones + operacion.getMontoComision();
       }
       
       return sumaComisiones;
     }
     
-    public static double LlenarTablaConsultaBancoPorCuenta(String cuenta)
+    public static void LlenarTablaConsultaBancoPorCuenta(String cuenta)
     {
       double sumaComisiones = 0;
       System.out.println("------------------------------------------------------------------------------------");
@@ -246,15 +225,13 @@ public class CLI {
       ArrayList<Operacion> operaciones = OperacionDAO.getOperacionesCuenta(cuenta);
       for(Operacion operacion: operaciones)
       {
-        if (operacion.getTipo().equals("retiro") && operacion.getTipo().equals("deposito"))
-        {
-          double monto = (operacion.getMontoComision()/0.02);
-          System.out.println("------------------------------------------------------------------------------------");
-          System.out.println("|"+operacion.getTipo()+"|-|"+operacion.getFechaOperacion()+"|-|"+ monto+"|-|"+operacion.getMontoComision()+"|");
-          sumaComisiones = sumaComisiones + operacion.getMontoComision();
-        }
+        double monto = (operacion.getMontoComision()/0.02);
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("|"+operacion.getTipo()+"|-|"+operacion.getFechaOperacion()+"|-|"+ monto+"|-|"+operacion.getMontoComision()+"|");
+        sumaComisiones = sumaComisiones + operacion.getMontoComision();
       }
-      return sumaComisiones;
+      System.out.println("------------------------------------------------------------------------------------");
+      System.out.println("LA SUMA TOTAL DE LA GANANCIA DEL BANCO ES DE:"+ sumaComisiones);
     }
     
     public static String retirar(String moneda)
